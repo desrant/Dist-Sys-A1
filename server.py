78ca2b752479d8fc4ac72dd964c060e4db5e6d5c
Rohan-18102001 <156566957+Rohan-18102001@users.noreply.github.com>
2024-03-07 18:13:11 +0530

    Update server.py

--- server.py
+++ server.py
@@ -3,7 +3,7 @@ import json
 from http.server import BaseHTTPRequestHandler, HTTPServer
 
 class SimpleServer(BaseHTTPRequestHandler):
-    server_id = os.getenv('SERVER_ID', 'default_server_id')  # Set a default value in case the environment variable is not set
+    server_id = os.getenv('SERVER_ID', 'server_0')  # Set a default value in case the environment variable is not set
 
     def do_GET(self):
         if self.path == '/home':
@@ -16,16 +16,36 @@ class SimpleServer(BaseHTTPRequestHandler):
             self.end_headers()
             self.wfile.write(json.dumps(response).encode())  # Use json.dumps to convert the dictionary to a JSON formatted string
         elif self.path == '/heartbeat':
+            response = {
+                "message": f"Hello from Server: {self.server_id}",
+                "status": "successful"
+            }
             self.send_response(200)
             self.end_headers()
-            self.wfile.write(b'')  # Send an empty byte string to ensure HTTP response format is correct
+            self.wfile.write(json.dumps(response).encode())  # Send an empty byte string to ensure HTTP response format is correct
+from flask import Flask, jsonify
+import os
 
+app = Flask(__name__)
+server_id = os.getenv('SERVER_ID', 'server_0')  # Set a default value in case the environment variable is not set
 
-def run(server_class=HTTPServer, handler_class=SimpleServer, port=5000):
-    server_address = ('', port)
-    httpd = server_class(server_address, handler_class)
-    print(f'Starting httpd server on port {port}')
-    httpd.serve_forever()
+@app.route('/home', methods=['GET'])
+def home():
+    response = {
+        "message": f"Hello from Server: {server_id}",
+        "status": "successful"
+    }
+    return jsonify(response), 200
 
-if __name__ == "__main__":
-    run()
+@app.route('/heartbeat', methods=['GET'])
+def heartbeat():
+    response = {
+        "message": f"Hello from Server: {server_id}",
+        "status": "successful"
+    }
+    return jsonify(response), 200
+
+if __name__ == '__main__':
+    # Define the port on which you want to run the server. Default is 5000.
+    port = int(os.getenv('PORT', 8000))
+    app.run(debug=True, host='0.0.0.0', port=port)
